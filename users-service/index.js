9be5b8a4b5e8fcdf82b97293e725ce3879e4ef88
lorenna velasquez <lorennavelasquez@MacBook-Air-de-lorenna.local>
2026-05-11 12:40:19 -0500

    nuevo cambio

--- users-service/index.js
+++ users-service/index.js
@@ -4,13 +4,13 @@ app.use(express.json());
 
 // Base de datos  de talleres mecánicos
 const clientes = [
-    { id: 1, taller: "Mecánica Juan", contacto: "3101234567", tipo: "Crédito 8 días" },
-    { id: 2, taller: "Taller Express", contacto: "3209876543", tipo: "Crédito 15 días" }
+    { id: 1, taller: 'Mecánica Juan', contacto: '3101234567', tipo: 'Crédito 8 días' },
+    { id: 2, taller: 'Taller Express', contacto: '3209876543', tipo: 'Crédito 15 días' }
 ];
 
 // Ruta para obtener todos los clientes
 app.get('/users', (req, res) => {
-    console.log("Consultando clientes de DistriHerramientas...");
+    console.log('Consultando clientes de DistriHerramientas...');
     res.json(clientes);
 });
 
@@ -18,7 +18,7 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
     const nuevoCliente = req.body;
     clientes.push(nuevoCliente);
-    res.json({ mensaje: "Cliente registrado con éxito", cliente: nuevoCliente });
+    res.json({ mensaje: 'Cliente registrado con éxito', cliente: nuevoCliente });
 });
 
 const PORT = 3001;
